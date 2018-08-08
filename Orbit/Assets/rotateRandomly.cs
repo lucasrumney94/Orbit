c2c3b220a9e198c01764dfac45a1f6e4bc19e3d4
Lucas Rumney <lucasrumney94@gmail.com>
2018-08-07 17:16:11 -0700

    add crystalline entity

---
+++ Orbit/Assets/rotateRandomly.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotateRandomly : MonoBehaviour {
+
+	public float speed = 5.0f;
+	public float amplitude = 10.0f;
+
+	private Vector3 direction;
+
+	// Use this for initialization
+	void Start () 
+	{
+		direction = Random.insideUnitSphere*speed*Time.deltaTime;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		transform.Rotate(direction*amplitude*Mathf.Sin(speed*Time.time));
+	}
+}
